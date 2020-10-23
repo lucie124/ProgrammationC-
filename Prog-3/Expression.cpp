464597fa4d52382642d510fa8b6eed29c84433cb
Zhaoqi GE <gezhaoqi@Titanes-MacBook-Pro.local>
2020-10-23 15:06:19 +0200

    J'ai rangé les codes. Clonage à travailler.

--- Prog-3/Expression.cpp
+++ Prog-3/Expression.cpp
@@ -1,13 +1,100 @@
 #include "Expression.hpp"
 
-// ostream & operator << (ostream & out, Expression * e )
-// {
-//     e->affiche(out);
-//     return out;
-// }
-
 ostream& operator<<( ostream& out, Expression const & e )
 {
         e.affiche(out);
         return out;
+}
+
+/*
+** Constructeurs
+*/
+Nombre::Nombre(int n){
+    valeur = n;
+    // cout << "constructeur Nombre" << endl;
+    // compteur +=1;
+    // compteur2 +=1;
+}
+
+Variable::Variable(string s){
+    nom = s;
+    // cout << "constructeur Variable" << endl;
+    // compteur +=1;
+    // compteur2 +=1;
+}
+
+Addition::Addition(Expression * a1, Expression * a2){
+    somme[0] = a1;
+    somme[1] = a2;
+    // cout << "constructeur Addition" << endl;
+}
+
+Multiplication::Multiplication(Expression * a1, Expression * a2){
+    produit[0] = a1;
+    produit[1] = a2;
+    // cout << "constructeur Multiplication" << endl;
+}
+
+/*
+** Destructeurs
+*/
+Nombre::~Nombre(){
+    // cout << "destructeur Nombre" << endl;
+    // compteur -=1;
+}
+
+Variable::~Variable(){
+    // cout << "destructeur Variable" << endl;
+    // compteur -=1;
+}
+
+Addition::~Addition(){
+    // cout << "destructeur Addition" << endl;
+}
+
+Multiplication::~Multiplication{
+    // cout << "destructeur Multiplication" << endl;
+}
+
+/*
+** Affichages
+*/
+ostream & Nombre::affiche(ostream & out) const {
+    return out << valeur;
+}
+
+ostream & Variable::affiche(ostream & out) const{
+    return out << nom;
+}
+
+ostream & Addition::affiche(ostream & out) const{
+    return out << "(" << *somme[0] << " + " << *somme[1] << ")";
+}
+
+ostream & Multiplication::affiche(ostream & out) const{
+    return out << "(" << *produit[0] << " * " << *produit[1] << ")";
+}
+
+/*
+** Derivations
+*/
+Nombre * Nombre::derive( string v ) const {
+    return new Nombre(0);
+}
+
+Nombre * Variable::derive( string v ) const {
+    if(nom == v){
+        return new Nombre(1);
+    }
+    else{
+        return new Nombre(0);
+    }
+}
+
+Expression * Addition::derive(string v) const{
+    return new Addition(somme[0]->derive(v) , somme[1]->derive(v));
+}
+
+Expression * Multiplication::derive(string v) const{
+    return new Addition(new Multiplication(produit[0]->derive(v), produit[1]), new Multiplication(produit[0], produit[1]->derive(v)));
 }
--- Prog-3/Expression.hpp
+++ Prog-3/Expression.hpp
@@ -12,7 +12,6 @@ public:
     Expression(){
         // cout << "constructeur Expression" << endl;
     }
-
     virtual ~Expression(){
         // cout << "destructeur Expression" << endl;
     }
@@ -27,57 +26,22 @@ public:
 
 class Nombre : public Expression {
 public:
-    Nombre(int n){
-        valeur = n;
-        // cout << "constructeur Nombre" << endl;
-        // compteur +=1;
-        // compteur2 +=1;
-    }
-
-    ~Nombre(){
-        // cout << "destructeur Nombre" << endl;
-        // compteur -=1;
-    }
-
-    ostream & affiche(ostream & out) const {
-        return out << valeur;
-    }
-
-    Nombre * derive( string v ) const {
-        return new Nombre(0);
-    }
+    Nombre(int n);
+    ~Nombre();
+    ostream & affiche(ostream & out) const;
+    Nombre * derive( string v ) const;
 
 private:
     int valeur;
-
 };
 
 class Variable : public Expression {
 public:
-    Variable(string s){
-        nom = s;
-        // cout << "constructeur Variable" << endl;
-        // compteur +=1;
-        // compteur2 +=1;
-    }
+    Variable(string s);
+    ~Variable();
+    ostream & affiche(ostream & out) const;
+    Nombre * derive( string v ) const;
 
-    ~Variable(){
-        // cout << "destructeur Variable" << endl;
-        // compteur -=1;
-    }
-
-    ostream & affiche(ostream & out) const{
-        return out << nom;
-    }
-
-    Nombre * derive( string v ) const {
-        if(nom == v){
-            return new Nombre(1);
-        }
-        else{
-            return new Nombre(0);
-        }
-    }
 private:
     string nom;
 };
@@ -88,37 +52,17 @@ public:
     Operation(){
         // cout << "constructeur Operation" << endl;
     }
-
     virtual ~Operation(){
         // cout << "destructeur Operation" << endl;
     }
-
-    // friend ostream& operator<<( ostream& out, Expression const & e );
-    // virtual ostream & affiche(ostream & out) const = 0;
-
-    // virtual Expression * derive(string v) const = 0;
-
 };
 
 class Addition : public Operation {
 public:
-    Addition(Expression * a1, Expression * a2){
-        somme[0] = a1;
-        somme[1] = a2;
-        // cout << "constructeur Addition" << endl;
-    }
-
-    ~Addition(){
-        // cout << "destructeur Addition" << endl;
-    }
-
-    ostream & affiche(ostream & out) const{
-        return out << "(" << *somme[0] << " + " << *somme[1] << ")";
-    }
-    
-    Expression * derive(string v) const{
-        return new Addition(somme[0]->derive(v) , somme[1]->derive(v));
-    }
+    Addition(Expression * a1, Expression * a2);
+    ~Addition();
+    ostream & affiche(ostream & out) const;
+    Expression * derive(string v) const;
 
 private:
     Expression * somme[2];
@@ -126,23 +70,11 @@ private:
 
 class Multiplication : public Operation {
 public:
-    Multiplication(Expression * a1, Expression * a2){
-        produit[0] = a1;
-        produit[1] = a2;
-        // cout << "constructeur Multiplication" << endl;
-    }
-    
-    ~Multiplication(){
-        // cout << "destructeur Multiplication" << endl;
-    }
+    Multiplication(Expression * a1, Expression * a2);
+    ~Multiplication();
+    ostream & affiche(ostream & out) const;
+    Expression * derive(string v) const;
 
-    ostream & affiche(ostream & out) const{
-        return out << "(" << *produit[0] << " * " << *produit[1] << ")";
-    }
-
-    Expression * derive(string v) const{
-        return new Addition(new Multiplication(produit[0]->derive(v), produit[1]), new Multiplication(produit[0], produit[1]->derive(v)));
-    }
 private:
     Expression * produit[2];
 };
